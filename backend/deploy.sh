e5a2f639c3651454183a2cc5001564a7a57c0d07
yisak mebrate <108977337+yisMeb@users.noreply.github.com>
2024-11-14 15:40:19 +0300

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -15,7 +15,10 @@ sudo mv env .env
 
 sudo apt-get update
 echo "installing python and pip"
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 
 # Install application dependencies from requirements.txt
 echo "Install application dependencies from requirements.txt"
