dd6e353b3f9b2a8e0b43c6b2d68b410dec0c2bb2
yisak mebrate <108977337+yisMeb@users.noreply.github.com>
2024-11-15 19:32:18 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,7 +37,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=[
+        "http://localhost:5173",
+        "https://main.d1bsmqmctjmx5w.amplifyapp.com/"
+        ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
