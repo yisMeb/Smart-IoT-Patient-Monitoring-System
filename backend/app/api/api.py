218a41c493a69e14d664963c2d80458e386b2058
Ekram Nurhussen <88303005+EkramNurhussen@users.noreply.github.com>
2024-11-16 17:49:10 +0300

    pushh

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,9 +1,14 @@
 
 from .routes.auth import router as auth_router
 from fastapi import APIRouter
+from .routes.institute import router as institution_router 
+
 
 
 api_router = APIRouter()
 
 # Include authentication routes
 api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
+
+# Include institution routes
+api_router.include_router(institution_router, prefix="/institutions",tags=["institutions"])
